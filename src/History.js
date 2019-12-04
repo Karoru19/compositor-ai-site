a524c288d5bd094a5b58bcd7be7a4377cf6fe01a
szymon <sbakunowicz@9bits.pl>
2019-12-04 22:27:31 +0100

    history

--- src/History.js
+++ src/History.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import 'typeface-roboto';
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
@@ -17,121 +17,146 @@ import PlayIcon from '@material-ui/icons/PlayCircleFilled';
 import PauseIcon from '@material-ui/icons/PauseCircleFilled';
 import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import Container from "@material-ui/core/Container";
-import CssBaseline from "@material-ui/core/CssBaseline";
 
-export default function History() {
-    const classes = useStyles();
-    const [dense, setDense] = React.useState(false);
-    const [secondary, setSecondary] = React.useState(false);
-    const [pause, setPause] = React.useState([-1]);
+export default class History extends Component {
 
-
-    const playButtonClick = value => () => {
-        const currentIndex = pause.indexOf(value);
-        const newPause = [...pause];
-
-        if (currentIndex === -1) {
-          newPause.push(value);
-        } else {
-          newPause.splice(currentIndex, 1);
-        }
-
-        setPause(newPause);
+    state = {
+        pause: [-1, -1, -1, -1],
+        songs: [0, 1, 2, 3]
     };
 
+    setPause = index => {
+
+        const newPause = [...this.state.pause];
+
+        if (newPause[index] === -1) {
+            newPause[index] = 1;
+        } else {
+            newPause[index] = -1;
+        }
+
+        console.log(newPause);
+
+        this.setState({pause: newPause});
+    };
+
+    downloadHandleEvent = index => {
+        console.log(index);
+    };
+
+    deleteSong = index => {
+        var array = [...this.state.songs]; // make a separate copy of the array
+        var arrayIndex = array.indexOf(index)
+        if (arrayIndex !== -1) {
+            array.splice(arrayIndex, 1);
+            this.setState({songs: array});
+        }
+        console.log(this.state.songs);
+    };
+
+    render() {
+        return (
+            <Container component="main">
+                <Typography component="h1" variant="h5">
+                    Your songs:
+                </Typography>
+                <Grid item xs={12} md={12}>
+                    <HistoryDiv deleteSongHandler={this.deleteSong} playButtonHandleEvent={this.setPause}
+                                downloadHandleEvent={this.downloadHandleEvent}
+                                songs={this.state.songs} pause={this.state.pause}/>
+                </Grid>
+            </Container>
+        );
+    }
+}
+
+
+const HistoryDiv = ({deleteSongHandler, playButtonHandleEvent, downloadHandleEvent, songs, pause}) => {
+    const classes = useStyles();
+
+
     return (
-        <Container component="main" >
-            <Typography component="h1" variant="h5">
-                Your songs:
-            </Typography>
-            <Grid item xs={12} md={12}>
-                <div className={classes.demo}>
-                    <List dense={dense}>
-                        {[0, 1, 2, 3].map(value =>{
-                          return (
-                            <ListItem>
-                              <ListItemAvatar>
+        <div className={classes.demo}>
+            <List>
+                {songs.map(value => {
+                    return (
+                        <ListItem key={value}>
+                            <ListItemAvatar>
                                 <Avatar>
-                                  <FolderIcon />
+                                    <FolderIcon/>
                                 </Avatar>
-                              </ListItemAvatar>
-                              <ListItemText
+                            </ListItemAvatar>
+                            <ListItemText
                                 primary="Song"
-                                secondary={secondary ? 'Secondary text' : null}
-                              />
-                              <ListItemSecondaryAction>
-                                <Button onClick={playButtonClick(value)}>
-                                  <PlayIcon style={pause.indexOf(value) !== -1 ? {} : {display: "none"}}/>
-                                  <PauseIcon style={pause.indexOf(value) !== -1 ? {display: "none"} : {}}/>
+                                secondary='Secondary text'
+                            />
+                            <ListItemSecondaryAction>
+                                <Button onClick={() => playButtonHandleEvent(value)}>
+                                    <PlayIcon style={pause[value] !== -1 ? {} : {display: "none"}}/>
+                                    <PauseIcon style={pause[value] !== -1 ? {display: "none"} : {}}/>
                                 </Button>
-                                <Button>
-                                  <DeleteIcon />
+                                <Button onClick={() => deleteSongHandler(value)}>
+                                    <DeleteIcon/>
                                 </Button>
-                                <Link href={"/download"}>
-                                  <Button>
-                                    <DownloadIcon />
-                                  </Button>
+                                <Link>
+                                    <Button
+                                        onClick={() => downloadHandleEvent(value)}>
+                                        <DownloadIcon/>
+                                    </Button>
                                 </Link>
-                              </ListItemSecondaryAction>
-                            </ListItem>
-                          );
-                        })}
-                    </List>
-                </div>
-            </Grid>
-        </Container>
+                            </ListItemSecondaryAction>
+                        </ListItem>
+                    );
+                })}
+            </List>
+        </div>
     );
 }
 
-const theme = {
-    spacing: 8,
-}
-
-
 const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-    maxWidth: 752,
-    margin: theme.spacing(8,4),
-  },
-  demo: {
-    backgroundColor: theme.palette.background.paper,
-  },
-  image: {
-    backgroundImage: 'url(https://source.unsplash.com/random)',
-    backgroundRepeat: 'no-repeat',
-    backgroundColor:
-      theme.palette.type === 'dark' ? theme.palette.grey[900] : theme.palette.grey[50],
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-  },
-  title: {
-    margin: theme.spacing(4, 0, 2),
-  },
-  commandButtons: {
-    display: 'flex',
-  },
-  dialogContent:{
-    padding: theme.spacing(20,0,0,0)
-  },
-  paper: {
-    margin: theme.spacing(8, 4),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-  upload:{
-    margin: theme.spacing(2.7,0),
-  }
+    root: {
+        flexGrow: 1,
+        maxWidth: 752,
+        margin: theme.spacing(8, 4),
+    },
+    demo: {
+        backgroundColor: theme.palette.background.paper,
+    },
+    image: {
+        backgroundImage: 'url(https://source.unsplash.com/random)',
+        backgroundRepeat: 'no-repeat',
+        backgroundColor:
+            theme.palette.type === 'dark' ? theme.palette.grey[900] : theme.palette.grey[50],
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+    },
+    title: {
+        margin: theme.spacing(4, 0, 2),
+    },
+    commandButtons: {
+        display: 'flex',
+    },
+    dialogContent: {
+        padding: theme.spacing(20, 0, 0, 0)
+    },
+    paper: {
+        margin: theme.spacing(8, 4),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+    upload: {
+        margin: theme.spacing(2.7, 0),
+    }
 }));
